3e1a6cfd83091eb19d98cf509a67d15c784ad967
Davi Vieira <davifmvieira@gmail.com>
2024-06-06 17:48:32 -0300

    Ajustes

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('login').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const msg_erro = document.getElementById('error-message');
+
+    if (username === '1234' && password === '1234') {
+        window.location.href = 'landing.html';
+    } else {
+        msg_erro.textContent = 'Nome de usuário ou senha incorretos.';
+        msg_erro.style.display = 'block';
+    }
+});
